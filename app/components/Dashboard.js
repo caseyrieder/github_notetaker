0b94f87fc9945c3134589513bae87030f3b89872
Casey Rieder <casey.rieder@gmail.com>
2016-08-04 15:06:51 -0400

    created buttons, reused btn styles, prepped for new pages

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableHighlight } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -17,11 +17,57 @@ const styles = StyleSheet.create({
 })
 
 class Dashboard extends Component {
-  render(){
+  makeBackground(btn) {
+    const obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+    if (btn === 0) {
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1) {
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
+  }
+
+  goToProfile() {
+    console.log('Go to Profile Page');
+  }
+
+  goToRepos() {
+    console.log('Go to Repos Page');
+  }
+
+  goToNotes() {
+    console.log('Go to Notes Page');
+  }
+
+  render() {
     return (
       <View style={styles.container}>
-        <Text> This is the dashboard </Text>
-        <Text> {this.props.userInfo.location} </Text>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+          underlayColor='#88D4F5'>
+          <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+          underlayColor='#88D4F5'>
+          <Text style={styles.buttonText}> View Repos </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(2)}
+          onPress={this.goToNotes.bind(this)}
+          underlayColor='#88D4F5'>
+          <Text style={styles.buttonText}> View Notes </Text>
+        </TouchableHighlight>
       </View>
     )
   }
