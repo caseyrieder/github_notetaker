876276dfc3df9581c07989e32bed7e89055344c4
Casey Rieder <casey.rieder@gmail.com>
2016-08-04 16:55:02 -0400

    set up firebase account & creted 2 api methods to get & post notes to firebase

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,16 +1,31 @@
 const api = {
+  // format username, set user's full github info, fetch & return as json
   getBio(username) {
-    // format username entered, set user's github home page, fetch that page & return it as json
     username = username.toLowerCase().trim();
     const url = `https://api.github.com/users/${username}`;
     return fetch(url).then((res) => res.json());
   },
+  // format username, set user's github repos page, fetch & return as json
   getRepos(username) {
-    // format username entered, set user's github repos page, fetch that page & return it as json
     username = username.toLowerCase().trim();
     const url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
   },
+  // format username, fetch firebase account, return as json
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    const url = `https://egghead-rn-github-app.firebaseio.com/${username}.json`;
+    return fetch(url).then((res) => res.json());
+  },
+  // format username, post stringified note to firebase, return as json
+  addNote(username, note) {
+    username = username.toLowerCase().trim();
+    const url = `https://egghead-rn-github-app.firebaseio.com/${username}.json`;
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note),
+    }).then((res) => res.json());
+  }
 };
 
 export default api;
