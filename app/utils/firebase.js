695b4d0de1ee78f18478e463e1689c9179c354e4
Casey Rieder <casey.rieder@gmail.com>
2016-08-05 10:30:18 -0400

    added & commented out firebase file

---
+++ app/utils/firebase.js
@@ -0,0 +1,10 @@
+// import * as firebase from 'firebase'
+//
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCRRY5kMZ9YE7hmu6oUbGBsZ8GVeBx2rf4",
+//   authDomain: "egghead-rn-github-app.firebaseapp.com",
+//   databaseURL: "https://egghead-rn-github-app.firebaseio.com",
+//   storageBucket: "egghead-rn-github-app.appspot.com",
+// };
+//
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
