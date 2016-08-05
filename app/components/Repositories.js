c1b4d893faad115511dbbae9d7f18e58d2750792
Casey Rieder <casey.rieder@gmail.com>
2016-08-05 10:36:35 -0400

    fixed repositories ui

--- app/components/Repositories.js
+++ app/components/Repositories.js
@@ -45,14 +45,16 @@ class Repositories extends Component {
       const desc = repos[index].description ? <Text style={styles.description}> {repos[index].description} </Text>: <View />;
       return (
         <View key={index}>
-          <View style={styles.rowContainer}>
-            <TouchableHighlight
-              onPress={this.openPage.bind(this, repos[index].html_url)}
-              underlayColor='transparent'>
-              <Text style={styles.name}>{repos[index].name}</Text>
-            </TouchableHighlight>
-            <Text style={styles.stars}> Stars: {repos[index].stargazers_count} </Text>
-            {desc}
+          <View>
+            <View style={styles.rowContainer}>
+              <TouchableHighlight
+                onPress={this.openPage.bind(this, repos[index].html_url)}
+                underlayColor='transparent'>
+                <Text style={styles.name}>{repos[index].name}</Text>
+              </TouchableHighlight>
+              <Text style={styles.stars}> Stars: {repos[index].stargazers_count} </Text>
+              {desc}
+            </View>
           </View>
           <Separator />
         </View>
